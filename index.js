ff805f99bfddba40a75d70ca8be86ec840d8ec70
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-04 22:16:38 +0600

    env file added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.d7bt1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d7bt1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
