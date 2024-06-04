17fad3ca457d3e2fb299bc0ba1a4a59ba76f12f0
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-04 22:44:12 +0600

    services, news and testimonial get api added

--- index.js
+++ index.js
@@ -25,6 +25,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const services = client.db('smart-hr').collection("services");
+    const news = client.db("smart-hr").collection("news");
+    const testimonials = client.db('smart-hr').collection("testimonials");
+
+    app.get('/services', async(req, res) => {
+        const result = await services.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/news', async(req, res) => {
+        const result = await news.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/testimonials', async(req, res) => {
+        const result = await testimonials.find().toArray();
+        res.send(result);
+    })
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -32,7 +51,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
