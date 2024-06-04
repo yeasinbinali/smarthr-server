e372f11f384fe1a3fc1a4692e67089babf2d6f2a
Mohammed Yeasin <yeasinbinali@gmail.com>
2024-06-04 21:56:38 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// smart-hr
+// l1ZKSwIqq9W0y8kd
+
+app.use(express.json());
+app.use(cors());
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.d7bt1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Smart hr server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Smart hr server is running on port ${port}`)
+})
